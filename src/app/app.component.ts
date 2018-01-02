d658820aceea32589a156ad4eaf0ac7ad46db759
David Lavelle <david.lavelle@ammeon.com>
2018-01-02 14:14:50 +0000

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+// Other component properties removed
+  template: `
+  <h1>{{title}}</h1>
+  <p>How are you dong?</p>
+  `,
 })
+
 export class AppComponent {
-  title = 'app';
+
+  title = 'Tour of Heroes';
+  myObject = {
+    gender: 'male',
+    age: 33,
+    location: 'USA'
+  };
+
 }
